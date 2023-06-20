dd6bc3eb772ce9bba399c18e975494e8b426a171
Nicolas <nicolasgfe3@gmail.com>
2023-06-20 13:21:20 -0300

    Iniciando projeto.

---
+++ demo/src/test/java/com/nicolas/nicolasgfe/NicolasgfeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolas.nicolasgfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NicolasgfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
